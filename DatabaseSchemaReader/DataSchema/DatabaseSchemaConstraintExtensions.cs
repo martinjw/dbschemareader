ac669ee48a87767ccb9520eb5b7dcd1e58dd6ff7
Martinjw_cp <SND\Martinjw_cp>
2011-07-31 02:42:19 -0700

    - Added a fluent interface for creating the database schema tables, columns and simple constraints.

---
+++ DatabaseSchemaReader/DataSchema/DatabaseSchemaConstraintExtensions.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseSchemaReader.DataSchema
+{
+    /// <summary>
+    /// Extensions to enable schema to be created with a simple fluent interface
+    /// </summary>
+    public static class DatabaseSchemaConstraintExtensions
+    {
+
+        /// <summary>
+        /// Makes this column the primary key.
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddPrimaryKey(this DatabaseColumn databaseColumn)
+        {
+            return databaseColumn.AddPrimaryKey(null);
+        }
+
+        /// <summary>
+        /// Adds the primary key.
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <param name="primaryKeyName">Name of the primary key.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddPrimaryKey(this DatabaseColumn databaseColumn, string primaryKeyName)
+        {
+            if (databaseColumn == null) throw new ArgumentNullException("databaseColumn", "databaseColumn must not be null");
+            var table = databaseColumn.Table;
+            table.PrimaryKey = new DatabaseConstraint
+            {
+                ConstraintType = ConstraintType.PrimaryKey,
+                Name = primaryKeyName
+            };
+            table.PrimaryKey.Columns.Add(databaseColumn.Name);
+            databaseColumn.IsPrimaryKey = true;
+            return databaseColumn;
+        }
+
+        /// <summary>
+        /// Adds the identity.
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddIdentity(this DatabaseColumn databaseColumn)
+        {
+            if (databaseColumn == null) throw new ArgumentNullException("databaseColumn", "databaseColumn must not be null");
+            var table = databaseColumn.Table;
+            if (table.HasIdentityColumn && !databaseColumn.IsIdentity)
+            {
+                var existingIdentity = table.Columns.First(x => x.IsIdentity);
+                existingIdentity.IsIdentity = false;
+            }
+            databaseColumn.IsIdentity = true;
+            return databaseColumn;
+        }
+
+        /// <summary>
+        /// Adds a foreign key with a single column
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <param name="foreignKeyName">Name of the foreign key.</param>
+        /// <param name="foreignTableName">Name of the foreign table.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddForeignKey(this DatabaseColumn databaseColumn, string foreignKeyName, string foreignTableName)
+        {
+            if (databaseColumn == null) throw new ArgumentNullException("databaseColumn", "databaseColumn must not be null");
+            if (string.IsNullOrEmpty(foreignTableName)) throw new ArgumentNullException("foreignTableName", "foreignTableName must not be null");
+            var table = databaseColumn.Table;
+            var foreignKey = new DatabaseConstraint
+            {
+                ConstraintType = ConstraintType.ForeignKey,
+                Name = foreignKeyName,
+                RefersToTable = foreignTableName
+            };
+            table.ForeignKeys.Add(foreignKey);
+            databaseColumn.IsForeignKey = true;
+
+            //add the inverse relationship
+            var fkTable = table.DatabaseSchema.FindTableByName(foreignTableName);
+            if (fkTable != null) fkTable.ForeignKeyChildren.Add(table);
+
+            return databaseColumn;
+        }
+
+        /// <summary>
+        /// Adds a foreign key with a single column
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <param name="foreignKeyName">Name of the foreign key.</param>
+        /// <param name="foreignTable">The foreign table.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddForeignKey(this DatabaseColumn databaseColumn, string foreignKeyName, Func<IEnumerable<DatabaseTable>, DatabaseTable> foreignTable)
+        {
+            if (databaseColumn == null) throw new ArgumentNullException("databaseColumn", "databaseColumn must not be null");
+            if (foreignTable == null) throw new ArgumentNullException("foreignTable", "foreignTable must not be null");
+            var table = databaseColumn.Table;
+            var fkTable = foreignTable(table.DatabaseSchema.Tables);
+            return databaseColumn.AddForeignKey(foreignKeyName, fkTable.Name);
+        }
+
+        /// <summary>
+        /// Adds a foreign key with a single column (without a name)
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <param name="foreignTableName">Name of the foreign table.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddForeignKey(this DatabaseColumn databaseColumn, string foreignTableName)
+        {
+            return databaseColumn.AddForeignKey(null, foreignTableName);
+        }
+
+        /// <summary>
+        /// Makes this column a unique key.
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddUniqueKey(this DatabaseColumn databaseColumn)
+        {
+            return databaseColumn.AddUniqueKey(null);
+        }
+
+        /// <summary>
+        /// Adds a unique key.
+        /// </summary>
+        /// <param name="databaseColumn">The database column.</param>
+        /// <param name="uniqueKeyName">Name of the unique key.</param>
+        /// <returns></returns>
+        public static DatabaseColumn AddUniqueKey(this DatabaseColumn databaseColumn, string uniqueKeyName)
+        {
+            if (databaseColumn == null) throw new ArgumentNullException("databaseColumn", "databaseColumn must not be null");
+            var table = databaseColumn.Table;
+            var uk = new DatabaseConstraint
+                                         {
+                                             ConstraintType = ConstraintType.UniqueKey,
+                                             Name = uniqueKeyName
+                                         };
+            uk.Columns.Add(databaseColumn.Name);
+            table.UniqueKeys.Add(uk);
+            databaseColumn.IsUniqueKey = true;
+            return databaseColumn;
+        }
+    }
+}
---
+++ DatabaseSchemaReaderTest/DataSchema/ExtensionsTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using DatabaseSchemaReader.DataSchema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseSchemaReaderTest.DataSchema
+{
+    [TestClass]
+    public class ExtensionsTest
+    {
+        [TestMethod]
+        public void TestExtensions()
+        {
+            //a simple fluent interface for creating the schema in memory
+            var schema = new DatabaseSchema(null, null);
+            schema
+                .AddTable("Categories")
+                //chaining adding pk and identity
+                .AddColumn("CategoryId", "INT").AddPrimaryKey().AddIdentity()
+                //chaining from one column to next, with full access to the new column
+                .AddColumn("CategoryName", "VARCHAR", c => c.Length = 30)
+
+                //chaining from a column to the next table
+                .AddTable("Products")
+                .AddColumn("ProductId", "INT").AddIdentity().AddPrimaryKey("PK_PRODUCTS")
+                //add additional properties to column
+                .AddColumn("ProductName", "VARCHAR", c =>
+                                                         {
+                                                             c.Length = 30;
+                                                             c.Nullable = true;
+                                                         })
+                //adding a column directly
+                .AddColumn(new DatabaseColumn { Name = "Price", DbDataType = "DECIMAL", Nullable = true })
+                //adding a fk
+                .AddColumn("CategoryId", "INT")
+                    .AddForeignKey("FK_CATEGORY", tables => tables.Where(x => x.Name == "Categories").First());
+
+            //assert
+            Assert.AreEqual(2, schema.Tables.Count);
+
+            var cats = schema.FindTableByName("Categories");
+            Assert.AreEqual(2, cats.Columns.Count);
+            Assert.IsNotNull(cats.PrimaryKey);
+            Assert.IsNotNull(cats.PrimaryKeyColumn);
+            var pk = cats.PrimaryKeyColumn;
+            Assert.IsTrue(pk.IsIdentity);
+            Assert.AreEqual(1, cats.ForeignKeyChildren.Count);
+
+
+            var prods = schema.FindTableByName("Products");
+            Assert.AreEqual(4, prods.Columns.Count);
+            Assert.AreEqual(1, prods.ForeignKeys.Count);
+            var fk = prods.ForeignKeys[0];
+            Assert.AreEqual(cats, fk.ReferencedTable(schema));
+        }
+
+        [TestMethod]
+        public void UniqueKeysTest()
+        {
+            var schema = new DatabaseSchema(null, null);
+            schema
+                .AddTable("Categories")
+                .AddColumn("CategoryId", "INT").AddPrimaryKey().AddIdentity()
+                .AddColumn("CategoryName", "VARCHAR", c => c.Length = 30).AddUniqueKey("UK_NAME");
+
+            //assert
+            var cats = schema.FindTableByName("Categories");
+
+            Assert.AreEqual(1, cats.UniqueKeys.Count);
+            var uk = cats.UniqueKeys[0];
+            Assert.AreEqual("UK_NAME", uk.Name);
+            Assert.AreEqual("CategoryName", uk.Columns.Single());
+
+            var catName = cats.Columns.Find(c => c.Name == "CategoryName");
+            Assert.IsTrue(catName.IsUniqueKey);
+
+        }
+    }
+}
