4a6307eea553cbfbcc438eabeab04639ad617696
Martinjw_cp <SND\Martinjw_cp>
2012-07-02 09:28:36 -0700

    - Added Data.ScriptWriter, which wraps Data.Reader and InsertWriter into an easier interface (for creating INSERT scripts for a table's data).

---
+++ DatabaseSchemaReader/Data/ScriptWriter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using DatabaseSchemaReader.Conversion;
+using DatabaseSchemaReader.DataSchema;
+
+namespace DatabaseSchemaReader.Data
+{
+    /// <summary>
+    /// Reads a database table schema and data, and writes data to INSERT statements (for copies or backup)
+    /// </summary>
+    /// <remarks>
+    /// This wraps Reader and InsertWriter to provide a higher level API.
+    /// </remarks>
+    public class ScriptWriter
+    {
+        private int _pageSize = 1000;
+
+        /// <summary>
+        /// Gets or sets the maximum number of records returned. Default is 1000.
+        /// </summary>
+        /// <value>The size of the page.</value>
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set
+            {
+                if (value <= 0) throw new InvalidOperationException("Must be a positive number");
+                if (value > 10000) throw new InvalidOperationException("Value is too large - consider another method");
+                _pageSize = value;
+            }
+        }
+
+        /// <summary>
+        /// Include identity values in INSERTs
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if include identity; otherwise, <c>false</c>.
+        /// </value>
+        public bool IncludeIdentity { get; set; }
+
+
+        /// <summary>
+        /// Include BLOB in INSERTS. This is only practical for small blobs for certain databases (eg it works in SqlServer Northwind).
+        /// </summary>
+        /// <value><c>true</c> if include blobs; otherwise, <c>false</c>.</value>
+        public bool IncludeBlobs { get; set; }
+
+        private static SqlType FindSqlType(string providerName)
+        {
+            var sqlType = ProviderToSqlType.Convert(providerName);
+            return !sqlType.HasValue ? SqlType.SqlServer : sqlType.Value;
+        }
+
+        /// <summary>
+        /// Reads the table data and returns the INSERT statements
+        /// </summary>
+        /// <param name="databaseTable">The database table.</param>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="providerName">Name of the provider.</param>
+        /// <returns></returns>
+        public string ReadTable(DatabaseTable databaseTable, string connectionString, string providerName)
+        {
+            var r = new Reader(databaseTable, connectionString, providerName);
+            r.PageSize = PageSize;
+            var dt = r.Read();
+            var w = new InsertWriter(databaseTable, dt);
+            w.IncludeIdentity = IncludeIdentity;
+            w.IncludeBlobs = IncludeBlobs;
+            return w.Write(FindSqlType(providerName));
+        }
+
+        /// <summary>
+        /// Reads the table schema and data and returns the INSERT statements
+        /// </summary>
+        /// <param name="tableName">Name of the table.</param>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="providerName">Name of the provider.</param>
+        /// <returns></returns>
+        public string ReadTable(string tableName, string connectionString, string providerName)
+        {
+            var dr = new DatabaseReader(connectionString, providerName);
+            var databaseTable = dr.Table(tableName);
+
+            return ReadTable(databaseTable, connectionString, providerName);
+        }
+
+    }
+}
---
+++ DatabaseSchemaReaderTest/SqlGen/InsertWriterTests/ScriptWriterTest.cs
@@ -0,0 +1,65 @@
+﻿using DatabaseSchemaReader;
+using DatabaseSchemaReader.Data;
+using DatabaseSchemaReaderTest.IntegrationTests;
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestContext = System.Object;
+#endif
+
+namespace DatabaseSchemaReaderTest.SqlGen.InsertWriterTests
+{
+    [TestClass]
+    public class ScriptWriterTest
+    {
+        const string Providername = "System.Data.SqlClient";
+        const string ConnectionString = ConnectionStrings.Northwind;
+
+        private static DatabaseReader GetNortwindReader()
+        {
+            ProviderChecker.Check(Providername, ConnectionString);
+
+            return new DatabaseReader(ConnectionString, Providername);
+        }
+
+        [TestMethod]
+        public void TestInsertIntegration()
+        {
+            //arrange
+            var dbReader = GetNortwindReader();
+            var table = dbReader.Table("Customers");
+
+            var rdr = new ScriptWriter();
+
+            //act
+            var txt = rdr.ReadTable(table, ConnectionString, Providername);
+
+            //assert
+            Assert.IsTrue(txt.Contains("INSERT INTO [Customers]"), "Insert statments created");
+            Assert.IsTrue(txt.Contains("[CustomerID],  [CompanyName]"), "Insert names the columns");
+            Assert.IsTrue(txt.Contains("'ALFKI'"), "Data includes Alfreds Futterkiste");
+        }
+
+        [TestMethod]
+        public void TestInsertNoDirectDatabaseReaderIntegration()
+        {
+            //arrange
+            ProviderChecker.Check(Providername, ConnectionString);
+
+            var rdr = new ScriptWriter();
+
+            //act
+            var txt = rdr.ReadTable("Customers", ConnectionString, Providername);
+
+            //assert
+            Assert.IsTrue(txt.Contains("INSERT INTO [Customers]"), "Insert statments created");
+            Assert.IsTrue(txt.Contains("[CustomerID],  [CompanyName]"), "Insert names the columns");
+            Assert.IsTrue(txt.Contains("'ALFKI'"), "Data includes Alfreds Futterkiste");
+        }
+    }
+}
