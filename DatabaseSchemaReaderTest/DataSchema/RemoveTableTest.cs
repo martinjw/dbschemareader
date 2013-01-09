ed9dfd69d0e18bdd981d0979177dd1d4c84e07a4
Martinjw_cp <SND\Martinjw_cp>
2013-01-09 09:26:41 -0800

    - Add DatabaseSchema extension RemoveTable, which also removes ForeignKeys so the modified schema is still valid.

---
+++ DatabaseSchemaReaderTest/DataSchema/RemoveTableTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DatabaseSchemaReader.DataSchema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseSchemaReaderTest.DataSchema
+{
+    [TestClass]
+    public class RemoveTableTest
+    {
+        [TestMethod]
+        public void RemoveTable()
+        {
+            //arrange
+            var schema = new DatabaseSchema(null, null);
+            schema
+                .AddTable("Category")
+                .AddColumn<int>("CategoryId").AddPrimaryKey()
+                .AddColumn<string>("CategoryName").AddLength(50).AddNullable()
+
+                .AddTable("Product")
+                .AddColumn<int>("Id").AddPrimaryKey()
+                .AddColumn<int>("CategoryId").AddForeignKey("Category")
+
+                .AddTable("Vendor")
+                .AddColumn<int>("Id").AddPrimaryKey()
+                .AddColumn<int>("CategoryId").AddForeignKey("Category")
+               ;
+
+            //act
+            schema.RemoveTable("Category");
+
+            //assert
+            Assert.AreEqual(2, schema.Tables.Count);
+            var prod = schema.FindTableByName("Product");
+            Assert.AreEqual(0, prod.ForeignKeys.Count);
+            var categoryId = prod.FindColumn("CategoryId");
+            Assert.IsFalse(categoryId.IsForeignKey);
+        }
+    }
+}
