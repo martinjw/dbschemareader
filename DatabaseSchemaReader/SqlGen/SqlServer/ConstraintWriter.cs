c04ba37b65f6a97e686eb916866d0c5b2054dd60
Martinjw_cp <SND\Martinjw_cp>
2013-03-10 11:10:50 -0700

    - SqlGen/ Patch 14046 : When a primary key is scripted out for a uniqueidentifier, it needs to be nonclustered in SQL Server otherwise there's a massive performance hit.

--- DatabaseSchemaReader/SqlGen/SqlServer/ConstraintWriter.cs
+++ DatabaseSchemaReader/SqlGen/SqlServer/ConstraintWriter.cs
@@ -1,4 +1,5 @@
 ﻿using DatabaseSchemaReader.DataSchema;
+using System.Globalization;
 
 namespace DatabaseSchemaReader.SqlGen.SqlServer
 {
@@ -19,6 +20,24 @@ namespace DatabaseSchemaReader.SqlGen.SqlServer
         protected override bool IsSelfReferencingCascadeAllowed()
         {
             return false;
+        }
+		
+		public override string WritePrimaryKey()
+        {
+            if (Table.PrimaryKey == null) return null;
+            var columnList = GetColumnList(Table.PrimaryKey.Columns);
+
+            var pkName = ConstraintName(Table.PrimaryKey.Name);
+			string nonClustered = "";
+			if (Table.PrimaryKey.Columns.Count == 1 && Table.PrimaryKeyColumn.NetName.ToLower () == "guid")
+				nonClustered = "NON CLUSTERED ";
+
+            return string.Format(CultureInfo.InvariantCulture,
+                                 @"ALTER TABLE {0} ADD CONSTRAINT {1} PRIMARY KEY {2}({3})",
+                                 TableName(Table),
+                                 EscapeName(pkName),
+			                     nonClustered,
+                                 columnList) + SqlFormatProvider().LineEnding();
         }
 
         #endregion
