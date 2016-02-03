4bb9e6b2ca07530d5b136dacb14560a400c3614c
Martin Willey <martinjwilley@gmail.com>
2016-02-03 18:00:52 +0100

    Fix for Oracle/OCI potential error

--- DatabaseSchemaReaderTest/IntegrationTests/ProviderChecker.cs
+++ DatabaseSchemaReaderTest/IntegrationTests/ProviderChecker.cs
@@ -35,6 +35,12 @@ namespace DatabaseSchemaReaderTest.IntegrationTests
             {
                 Assert.Inconclusive("Cannot test for provider " + providerName);
             }
+            catch (System.Reflection.TargetInvocationException exception)
+            {
+                //The provider is not compatible with the version of Oracle client (32bit/64bit)
+                Assert.Inconclusive("Cannot access database for provider " + providerName +
+                    " message= " + exception.Message);
+            }
 
             try
             {
@@ -47,7 +53,8 @@ namespace DatabaseSchemaReaderTest.IntegrationTests
             }
             catch (DbException exception)
             {
-                Assert.Inconclusive("Cannot access database for provider " + providerName + " message= " + exception.Message);
+                Assert.Inconclusive("Cannot access database for provider " + providerName + " message= " +
+                                    exception.Message);
             }
             catch (Exception exception)
             {
