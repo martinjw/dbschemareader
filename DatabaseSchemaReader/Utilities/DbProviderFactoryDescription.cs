e12586aea7a963ece9832fdc7492b64fa0f86824
Martin Willey <martinjwilley@gmail.com>
2016-03-09 17:49:37 +0100

    Add DbProviderFactoryRepository to Utilities
    From th0masj0

---
+++ DatabaseSchemaReader/Utilities/DbProviderFactoryDescription.cs
@@ -0,0 +1,78 @@
+using System.Data;
+
+namespace DatabaseSchemaReader.Utilities
+{
+    /// <summary>
+    ///     Description of a DbProviderFactory for Repository.
+    /// </summary>
+    public class DbProviderFactoryDescription
+    {
+
+        /// <summary>
+        ///     Initialize the description.
+        /// </summary>
+        public DbProviderFactoryDescription() {}
+
+        /// <summary>
+        ///     Initialize the description.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
+        /// <param name="invariant"></param>
+        /// <param name="type"></param>
+        public DbProviderFactoryDescription(string name, string description, string invariant, string type)
+        {
+            Name = name;
+            Description = description;
+            InvariantName = invariant;
+            AssemblyQualifiedName = type;
+        }
+
+        /// <summary>
+        ///     Initialize the description based on a row.
+        /// </summary>
+        /// <param name="row">The row.</param>
+        internal DbProviderFactoryDescription(DataRow row)
+        {
+            Name = row["Name"].ToString();
+            Description = row["Description"].ToString();
+            InvariantName = row["InvariantName"].ToString();
+            AssemblyQualifiedName = row["AssemblyQualifiedName"].ToString();
+        }
+
+        /// <summary>
+        ///     Gets or sets the assemblyQualifiedName.
+        /// </summary>
+        /// <value>The assemblyQualifiedName.</value>
+        public string AssemblyQualifiedName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the invariantName.
+        /// </summary>
+        /// <value>The invariantName.</value>
+        public string InvariantName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return InvariantName;
+        }
+    }
+}
---
+++ DatabaseSchemaReaderTest/Utilities/DbProviderFactoryRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DatabaseSchemaReader.Utilities;
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+#endif
+
+namespace DatabaseSchemaReaderTest.Utilities
+{
+    [TestClass]
+    public class DbProviderFactoryRepositoryTest
+    {
+        [TestMethod]
+        public void TestRepository()
+        {
+            // Create a description manually and add it to the repository.
+            var manualDescription = new DbProviderFactoryDescription
+            {
+                Description = ".NET Framework Data Provider for SuperDuperDatabase",
+                InvariantName = "SuperDuperDatabase",
+                Name = "SuperDuperDatabase Data Provider",
+                AssemblyQualifiedName = "SuperDuperDatabase.SuperDuperProviderFactory, SuperDuperDatabase",
+            };
+
+            // Initialize the repository.
+            DbProviderFactoryRepository.Add(manualDescription);
+
+            var descs = DbProviderFactoryRepository.GetAllDescriptions();
+            foreach (var description in descs)
+            {
+                //get the description individually
+                var desc = DbProviderFactoryRepository.GetDescriptionByInvariant(description.InvariantName);
+                Assert.AreEqual(description.AssemblyQualifiedName, desc.AssemblyQualifiedName);
+
+                //get a factory
+                var factory = DbProviderFactoryRepository.GetFactory(desc);
+                //may be null if not accessible
+            }
+
+            //look in the current directory
+            DbProviderFactoryRepository.LoadExternalDbProviderAssemblies(Environment.CurrentDirectory);
+        }
+    }
+}
