3391c330aa6ffe8cf4878327d32779761b84dc0a
Martin Willey <martinjwilley@gmail.com>
2016-06-24 22:02:48 +0200

    Adding packOptions to project.json

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 :: %systemroot%\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe build.proj /t:Release /fl /flp:Verbosity=normal & pause
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" build.proj /t:Release /fl /flp:Verbosity=normal & pause
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" build.proj /t:Release /fl /flp:Verbosity=normal
+dotnet pack DatabaseSchemaReader -c Release -o nuget
+pause
