71463361ffbb8154a9a659a2fa6e8166ac4286d6
Martin Willey <martinjwilley@gmail.com>
2016-09-12 21:48:38 +0200

    Add INamedSchemaObject (see https://dbschemareader.codeplex.com/discussions/657891)

---
+++ DatabaseSchemaReader/DataSchema/INamedSchemaObject.cs
@@ -0,0 +1,18 @@
+﻿namespace DatabaseSchemaReader.DataSchema
+{
+    /// <summary>
+    /// A database object with name and schema
+    /// </summary>
+    /// <seealso cref="DatabaseSchemaReader.DataSchema.INamedObject" />
+    public interface INamedSchemaObject : INamedObject
+    {
+
+        /// <summary>
+        /// Gets or sets the schema/owner.
+        /// </summary>
+        /// <value>
+        /// The schema/owner.
+        /// </value>
+        string SchemaOwner { get; set; }
+    }
+}
