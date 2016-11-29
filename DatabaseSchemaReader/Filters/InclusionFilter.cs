0ad5faa5bd731a3f648504e3cc7f090502ba942a
Martin Willey <martinjwilley@gmail.com>
2016-11-29 20:14:05 +0100

    Fix #22. Inclusion filter

---
+++ DatabaseSchemaReader/Filters/InclusionFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace DatabaseSchemaReader.Filters
+{
+    /// <summary>
+    /// Include (rather than exclude)
+    /// </summary>
+    /// <seealso cref="DatabaseSchemaReader.Filters.Filter" />
+    public class InclusionFilter : Filter
+    {
+        /// <summary>
+        /// Excludes the specified name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public override bool Exclude(string name)
+        {
+            //inverted, so exclusion is really inclusion
+            return !FilterExclusions.Contains(name);
+        }
+    }
+}
