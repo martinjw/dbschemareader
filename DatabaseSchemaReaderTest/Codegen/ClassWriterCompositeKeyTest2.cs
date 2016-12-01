9554b7c4bb4e137705bdd1f8a43859a5ed57e5ff
Martin Willey <martinjwilley@gmail.com>
2016-12-01 19:54:34 +0100

    Fix #21 - incomplete foreign keys breaks codegen

--- DatabaseSchemaReaderTest/Codegen/ClassWriterCompositeKeyTest2.cs
+++ DatabaseSchemaReaderTest/Codegen/ClassWriterCompositeKeyTest2.cs
@@ -13,9 +13,17 @@ namespace DatabaseSchemaReaderTest.Codegen
         {
             //based on Github issue #21
             //arrange
+            //this schema model has a primary key which is also in 2 different foreign keys
+            //AND one of the foreign keys is not in the model (exclusions)
+            //this confused the code writer...
             var dept = Arrange();
 
-            var cw = new ClassWriter(dept, new CodeWriterSettings { CodeTarget = CodeTarget.PocoEntityCodeFirst});
+            var settings = new CodeWriterSettings
+            {
+                CodeTarget = CodeTarget.PocoEntityCodeFirst,
+                Namer = new PluralizingNamer()
+            };
+            var cw = new ClassWriter(dept, settings);
 
             //act
             var txt = cw.Write();
@@ -23,30 +31,51 @@ namespace DatabaseSchemaReaderTest.Codegen
             //assert
             //what should this return?
             Console.WriteLine(txt);
+            Assert.IsTrue(txt.IndexOf("public int BusinessUnitId { get; set; }", StringComparison.Ordinal) > 0,
+                "Should have xId scalar key");
+            Assert.IsTrue(txt.IndexOf("public virtual BusinessUnit BusinessUnit { get; set; }", StringComparison.Ordinal) > 0,
+                "Should have x reference property");
         }
 
         private static DatabaseTable Arrange()
         {
             var schema = new DatabaseSchema(null, SqlType.SqlServer);
-            schema.DataTypes.Add(new DataType("INT", "System.Int32"));
-            schema.DataTypes.Add(new DataType("NVARCHAR", "System.String"));
+            schema.DataTypes.Add(new DataType("NUMBER", "System.Int32"));
+            schema.DataTypes.Add(new DataType("VARCHAR2", "System.String"));
 
-            var bu = schema.AddTable("BusinessUnit")
-                .AddColumn("BusinessUnitId", "INT").AddPrimaryKey().AddIdentity()
-                .AddColumn("UnitName", "NVARCHAR")
+            var bu = schema.AddTable("BUSINESS_UNITS")
+                .AddColumn("BUSINESS_UNIT_ID", "NUMBER").AddPrimaryKey().AddIdentity()
+                .AddColumn("DESCRIPTION", "VARCHAR2")
                 .Table;
-            var dept = schema.AddTable("Department")
-                .AddColumn("DepartmentId", "INT").AddPrimaryKey()
-                .AddColumn("BusinessUnitId", "INT").AddForeignKey("FK", "BusinessUnit")
+            var dept = schema.AddTable("DEPARTMENTS")
+                .AddColumn("BUSINESS_UNIT_ID", "NUMBER")
+                .AddColumn("DEPARTMENT_ID", "VARCHAR2").AddLength(4)
+                .AddColumn("GROUP_ID", "NUMBER")
                 .Table;
-            var pk = new DatabaseConstraint {ConstraintType = ConstraintType.PrimaryKey};
-            pk.Columns.Add("DepartmentId");
-            pk.Columns.Add("BusinessUnitId");
+            var businessUnitFk = new DatabaseConstraint
+            {
+                ConstraintType = ConstraintType.ForeignKey,
+                RefersToTable = bu.Name
+            };
+            businessUnitFk.Columns.Add("BUSINESS_UNIT_ID");
+            dept.AddConstraint(businessUnitFk);
+            //This one IS NOT in the model, so there are broken fk links
+            var groupFk = new DatabaseConstraint
+            {
+                ConstraintType = ConstraintType.ForeignKey,
+                RefersToTable = "GROUPS"
+            };
+            groupFk.Columns.Add("BUSINESS_UNIT_ID");
+            groupFk.Columns.Add("DEPARTMENT_ID");
+            groupFk.Columns.Add("GROUP_ID");
+            dept.AddConstraint(groupFk);
+            var pk = new DatabaseConstraint { ConstraintType = ConstraintType.PrimaryKey };
+            pk.Columns.Add("BUSINESS_UNIT_ID");
+            pk.Columns.Add("DEPARTMENT_ID");
             dept.AddConstraint(pk);
 
             DatabaseSchemaFixer.UpdateDataTypes(schema);
-            //make sure .Net names are assigned
-            PrepareSchemaNames.Prepare(schema, new Namer());
+
             return dept;
         }
     }
