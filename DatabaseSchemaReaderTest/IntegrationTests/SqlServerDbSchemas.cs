22b048ac71bd3dcb2af17b5d92566fd35e46dde5
Martin Willey <martinjwilley@gmail.com>
2017-12-26 16:11:58 +0100

    Add a simple test for schemas

---
+++ DatabaseSchemaReaderTest/IntegrationTests/SqlServerDbSchemas.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DatabaseSchemaReaderTest.IntegrationTests
+{
+    [TestClass]
+    public class SqlServerDbSchemas
+    {
+        [TestMethod, TestCategory("SqlServer")]
+        public void DiscoverSqlServerDbSchemas()
+        {
+            var dbReader = TestHelper.GetNorthwindReader();
+            var schemas = dbReader.AllSchemas();
+
+            var dbo = schemas.FirstOrDefault(x => x.Name == "dbo");
+            Assert.IsNotNull(dbo);
+        }
+    }
+}
