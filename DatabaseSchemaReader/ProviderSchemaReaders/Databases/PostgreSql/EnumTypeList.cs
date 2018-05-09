e377dad06533b040c9571bfe727fc70301a6b7c3
Kyle Estes <kyle_estes@trimble.com>
2018-05-09 10:59:22 -0500

    Adding functionality for detecting enumeration types in the PostgreSQL database. TODO: use System.CodeDom to emit dynamically-compiled new C# types that can then be subsequently used.

---
+++ DatabaseSchemaReader/ProviderSchemaReaders/Databases/PostgreSql/EnumTypeList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using DatabaseSchemaReader.DataSchema;
+using DatabaseSchemaReader.ProviderSchemaReaders.ConnectionContext;
+
+namespace DatabaseSchemaReader.ProviderSchemaReaders.Databases.PostgreSql
+{
+    class EnumTypeList : SqlExecuter<DataType>
+    {
+        public EnumTypeList()
+        {
+            Sql = @"select * from pg_type pgt join pg_enum pge on pgt.oid = pge.enumtypid;";
+        }
+
+        protected override void AddParameters(DbCommand command)
+        {
+            //AddDbParameter(command, "OWNER", Owner);
+            //AddDbParameter(command, "TABLENAME", _tableName);
+        }
+
+        protected override void Mapper(IDataRecord record)
+        {
+            //var schema = record["table_schema"].ToString();
+            //var name = record["table_name"].ToString();
+            //var table = new DatabaseTable
+            //{
+            //    Name = name,
+            //    SchemaOwner = schema
+            //};
+
+            //Result.Add(table);
+
+        }
+
+        public IList<DataType> Execute(IConnectionAdapter connectionAdapter)
+        {
+            ExecuteDbReader(connectionAdapter);
+            return Result;            
+        }
+    }
+}
