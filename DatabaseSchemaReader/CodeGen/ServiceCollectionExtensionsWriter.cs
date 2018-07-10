3caf9987828ee7d61fd7e8a04491d2023ae4e6fd
Kyle Estes <kyle_estes@trimble.com>
2018-07-10 11:17:59 -0500

    Codegen now emits auto registration of each repostiroy

---
+++ DatabaseSchemaReader/CodeGen/ServiceCollectionExtensionsWriter.cs
@@ -0,0 +1,60 @@
+﻿using DatabaseSchemaReader.DataSchema;
+
+namespace DatabaseSchemaReader.CodeGen
+{
+    public class ServiceCollectionExtensionsWriter
+    {
+        private ClassBuilder classBuilder;
+        private CodeWriterSettings codeWriterSettings { get; }
+        private DatabaseSchema schema { get; }
+        private string className = "ServiceCollectionExtensions";
+
+        public ServiceCollectionExtensionsWriter(DatabaseSchema schema, CodeWriterSettings codeWriterSettings)
+        {
+            this.codeWriterSettings = codeWriterSettings;
+            this.schema = schema;
+            classBuilder = new ClassBuilder();
+        }
+
+        public void Execute()
+        {
+            var implementationText = Write();
+            CodeWriterUtils.WriteClassFile(codeWriterSettings.OutputDirectory, className, implementationText);
+        }
+
+        private string Write()
+        {
+            WriteUsings();
+            using (classBuilder.BeginNest($"namespace {codeWriterSettings.Namespace}"))
+            {
+                using (classBuilder.BeginNest($"public static class {className}"))
+                {
+                    using (classBuilder.BeginNest($"public static IServiceCollection AddEnterpriseDataRepositories(this IServiceCollection services)"))
+                    {
+                        foreach (var t in schema.Tables)
+                        {
+                            var interfaceName = CodeWriterUtils.GetRepositoryInterfaceName(t);
+                            var implementationName = CodeWriterUtils.GetRepositoryImplementationName(t);
+                            classBuilder.AppendLine($"services.AddTransient<{interfaceName}, {implementationName}>();");
+                        }
+
+                        classBuilder.AppendLine("return services;");
+                    }
+                }
+            }
+
+            return classBuilder.ToString();
+        }
+
+        private void WriteUsings()
+        {
+            classBuilder.AppendLine("using Microsoft.Extensions.DependencyInjection;");
+            foreach (var u in codeWriterSettings.Usings)
+            {
+                classBuilder.AppendLine($"using {u};");
+            }
+
+            classBuilder.AppendLine("");
+        }
+    }
+}
