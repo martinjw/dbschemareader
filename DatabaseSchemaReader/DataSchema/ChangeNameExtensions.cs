da3d8943f84b5b6f1706264a67a36a7bc577215e
Martin Willey <martinjwilley@gmail.com>
2020-04-04 16:07:53 +0200

    ChangeName extension for better table/col name changes

---
+++ DatabaseSchemaReader/DataSchema/ChangeNameExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseSchemaReader.DataSchema
+{
+    /// <summary>
+    /// Extensions to change names
+    /// </summary>
+    public static class ChangeNameExtensions
+    {
+        /// <summary>
+        /// Change the name of a table and references to it. (But not views or sprocs!)
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="newName">The new name of the table</param>
+        public static void ChangeName(this DatabaseTable table, string newName)
+        {
+            if (table == null) return;
+            if (string.IsNullOrEmpty(newName)) return;
+
+            var oldName = table.Name;
+            table.Name = newName;
+
+            //most of these are convenience
+            foreach (var column in table.Columns)
+            {
+                column.TableName = newName;
+            }
+
+            if (table.PrimaryKey != null) table.PrimaryKey.TableName = newName;
+            foreach (var chk in table.CheckConstraints)
+            {
+                chk.TableName = newName;
+            }
+            foreach (var uniqueKey in table.UniqueKeys)
+            {
+                uniqueKey.TableName = newName;
+            }
+            foreach (var def in table.DefaultConstraints)
+            {
+                def.TableName = newName;
+            }
+            foreach (var index in table.Indexes)
+            {
+                index.TableName = newName;
+            }
+
+            foreach (var fk in table.ForeignKeys)
+            {
+                fk.TableName = newName;
+            }
+            //important
+            foreach (var dep in table.ForeignKeyChildren)
+            {
+                foreach (var fk in dep.ForeignKeys)
+                {
+                    if (string.Equals(oldName, fk.RefersToTable, StringComparison.OrdinalIgnoreCase))
+                    {
+                        fk.RefersToTable = newName;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Change the name of a table and references to it. (But not views or sprocs!)
+        /// </summary>
+        /// <param name="column"></param>
+        /// <param name="newName">The new name of the column</param>
+        public static void ChangeName(this DatabaseColumn column, string newName)
+        {
+            if (column == null) return;
+            if (string.IsNullOrEmpty(newName)) return;
+
+            var oldName = column.Name;
+            column.Name = newName;
+
+            var table = column.Table;
+            if (table == null) return; //disconnected - can't do more
+
+            if (table.PrimaryKey != null)
+            {
+                ChangeConstraint(table.PrimaryKey.Columns, oldName, newName);
+            }
+            foreach (var fk in table.ForeignKeys)
+            {
+                ChangeConstraint(fk.Columns, oldName, newName);
+            }
+            foreach (var uk in table.UniqueKeys)
+            {
+                ChangeConstraint(uk.Columns, oldName, newName);
+            }
+        }
+
+        private static void ChangeConstraint(List<string> columns, string oldName, string newName)
+        {
+            var index = columns.FindIndex(x => x == oldName);
+            if (index == -1) return;
+            columns[index] = newName;
+        }
+    }
+}
---
+++ DatabaseSchemaReaderTest/DataSchema/ChangeNameTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using DatabaseSchemaReader.DataSchema;
+using DatabaseSchemaReader.SqlGen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseSchemaReaderTest.DataSchema
+{
+    [TestClass]
+    public class ChangeNameTest
+    {
+        [TestMethod]
+        public void ChangeTableNames()
+        {
+            var schema = BuildSchema();
+
+            var cats = schema.FindTableByName("Categories");
+            cats.ChangeName("DOGS");
+
+            var prods = schema.FindTableByName("Products");
+            prods.ChangeName("SPIDERS");
+
+            var gen = new DdlGeneratorFactory(SqlType.Oracle);
+            var tabgen = gen.AllTablesGenerator(schema);
+            tabgen.IncludeSchema = false;
+            var s = tabgen.Write();
+            
+            Assert.IsTrue(s.Contains("DOGS"));
+            Assert.IsTrue(s.Contains("SPIDERS"));
+            Assert.IsFalse(s.Contains("Categories"));
+            Assert.IsFalse(s.Contains("Products"));
+        }
+
+        [TestMethod]
+        public void ChangeColumnNames()
+        {
+            var schema = BuildSchema();
+
+            var cats = schema.FindTableByName("Categories");
+            cats.ChangeName("DOGS");
+            cats.FindColumn("CategoryId").ChangeName("DogId");
+
+            var prods = schema.FindTableByName("Products");
+            prods.FindColumn("CategoryId").ChangeName("DogId");
+
+            var gen = new DdlGeneratorFactory(SqlType.Oracle);
+            var tabgen = gen.AllTablesGenerator(schema);
+            tabgen.IncludeSchema = false;
+            var s = tabgen.Write();
+
+            Assert.IsTrue(s.Contains("DogId"));
+            Assert.IsFalse(s.Contains("CategoryId"));
+        }
+
+
+        private static DatabaseSchema BuildSchema()
+        {
+            var schema = new DatabaseSchema(null, "Dummy");
+            schema
+                .AddTable("Categories")
+                //chaining adding pk and identity
+                .AddColumn("CategoryId", "INT").AddPrimaryKey().AddIdentity()
+                //chaining from one column to next, with full access to the new column
+                .AddColumn("CategoryName", "VARCHAR", c => c.Length = 30)
+                .AddIndex("IndexByName")
+
+                //chaining from a column to the next table
+                .AddTable("Products")
+                .AddColumn("ProductId", "INT").AddIdentity().AddPrimaryKey("PK_PRODUCTS")
+                //add additional properties to column
+                .AddColumn("ProductName", "VARCHAR", c =>
+                {
+                    c.Length = 30;
+                    c.Nullable = true;
+                })
+                //adding a column directly
+                .AddColumn(new DatabaseColumn {Name = "Price", DbDataType = "DECIMAL", Nullable = true})
+                //adding a fk
+                .AddColumn("CategoryId", "INT")
+                .AddForeignKey("FK_CATEGORY", tables => tables.Where(x => x.Name == "Categories").First());
+            return schema;
+        }
+    }
+}
