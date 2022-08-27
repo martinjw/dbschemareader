822ddc13b661e2941eb4b59c803abebbd9fa93eb
martinjw <martinwilley@hotmail.com>
2022-08-27 12:08:18 +0200

    Testing drop table

--- DatabaseSchemaReaderTest/SqlGen/Migrations/UnitTests/MigrationDropTable.cs
+++ DatabaseSchemaReaderTest/SqlGen/Migrations/UnitTests/MigrationDropTable.cs
@@ -1,4 +1,5 @@
-﻿using DatabaseSchemaReader.DataSchema;
+﻿using System.Linq;
+using DatabaseSchemaReader.DataSchema;
 using DatabaseSchemaReader.SqlGen;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,31 +9,32 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
     public class MigrationDropTable
     {
         [TestMethod]
-        public void TestSqlServer()
+        public void TestDropTable()
         {
             //arrange
-            var migration = new DdlGeneratorFactory(SqlType.SqlServer).MigrationGenerator();
+            var migration = new DdlGeneratorFactory(SqlType.PostgreSql).MigrationGenerator();
 
             var schema = new DatabaseSchema(null, null);
             var customerTable = new DatabaseTable { Name = "Customers" };
             schema.AddTable(customerTable);
-            customerTable.DatabaseSchema = schema;
-            schema.Tables.Add(customerTable);
             customerTable.AddColumn<int>("Id")
                 .AddPrimaryKey()
                 .AddColumn<string>("Name").AddLength(30);
-            var orderTable = new DatabaseTable { Name = "Requests" };
-            schema.AddTable(orderTable);
-            orderTable.AddColumn<int>("Id")
+            var requestsTable = new DatabaseTable { Name = "Requests" };
+            schema.AddTable(requestsTable);
+            requestsTable.AddColumn<int>("Id")
                 .AddPrimaryKey()
                 .AddColumn<int>("CustomerId").AddForeignKey("FK_Order_Customer","Customers")
                 .AddColumn<string>("Comment").AddLength(50);
 
             //act
+            var addCustomer = migration.AddTable(customerTable);
+            var addRequest = migration.AddTable(requestsTable);
+            var addConstraint = migration.AddConstraint(requestsTable, requestsTable.ForeignKeys.First());
             var sql = migration.DropTable(customerTable);
 
             //assert
-            Assert.IsTrue(sql.Contains("DROP TABLE [Customers];"), "should include the drop table");
+            Assert.IsTrue(sql.Contains("DROP TABLE IF EXISTS \"Customers\" CASCADE;"), "should include the drop table");
 
         }
     }
