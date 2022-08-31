ef4dc2c835cf2dbc74f69014ad830541b7e40765
martinjw <martinwilley@hotmail.com>
2022-08-31 13:45:36 +0200

    #139

--- DatabaseSchemaReaderTest/SqlGen/Migrations/UnitTests/MigrationDropColumn.cs
+++ DatabaseSchemaReaderTest/SqlGen/Migrations/UnitTests/MigrationDropColumn.cs
@@ -1,18 +1,16 @@
-using System;
 using DatabaseSchemaReader.DataSchema;
 using DatabaseSchemaReader.SqlGen;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
 {
     [TestClass]
     public class MigrationDropColumn
     {
-
         [TestMethod]
         public void TestSqlServerWithSchema()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.SqlServer).MigrationGenerator();
 
@@ -27,11 +25,9 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
             Assert.IsTrue(sql.StartsWith("ALTER TABLE [dbo].[Orders] DROP COLUMN [COUNTRY]", StringComparison.OrdinalIgnoreCase), "names should be quoted correctly");
         }
 
-
         [TestMethod]
         public void TestSqlServerNoSchema()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.SqlServer).MigrationGenerator();
 
@@ -50,7 +46,6 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
         [TestMethod]
         public void TestSqlServerNoSchemaNoEscapeNames()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.SqlServer).MigrationGenerator();
             migration.EscapeNames = false;
@@ -70,7 +65,6 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
         [TestMethod]
         public void TestOracleWithSchema()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.Oracle).MigrationGenerator();
 
@@ -85,11 +79,9 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
             Assert.IsTrue(sql.StartsWith("ALTER TABLE \"dbo\".\"Orders\" DROP COLUMN \"COUNTRY\"", StringComparison.OrdinalIgnoreCase), "names should be quoted correctly");
         }
 
-
         [TestMethod]
         public void TestOracleNoSchema()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.Oracle).MigrationGenerator();
 
@@ -108,7 +100,6 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
         [TestMethod]
         public void TestMySqlWithSchema()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.MySql).MigrationGenerator();
 
@@ -123,11 +114,9 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
             Assert.IsTrue(sql.StartsWith("ALTER TABLE `dbo`.`Orders` DROP COLUMN `COUNTRY`", StringComparison.OrdinalIgnoreCase), "names should be quoted correctly");
         }
 
-
         [TestMethod]
         public void TestMySqlNoSchema()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.MySql).MigrationGenerator();
 
@@ -143,11 +132,9 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
             Assert.IsTrue(sql.StartsWith("ALTER TABLE `Orders` DROP COLUMN `COUNTRY`", StringComparison.OrdinalIgnoreCase), "names should be quoted correctly");
         }
 
-
         [TestMethod]
         public void TestSqLite()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.SQLite).MigrationGenerator();
 
@@ -162,11 +149,9 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
             Assert.IsTrue(sql.StartsWith("--", StringComparison.OrdinalIgnoreCase), "Not available in SQLite");
         }
 
-
         [TestMethod]
         public void TestDb2()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.Db2).MigrationGenerator();
 
@@ -184,7 +169,6 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
         [TestMethod]
         public void TestPostgreSql()
         {
-
             //arrange
             var migration = new DdlGeneratorFactory(SqlType.PostgreSql).MigrationGenerator();
 
@@ -197,6 +181,27 @@ namespace DatabaseSchemaReaderTest.SqlGen.Migrations.UnitTests
 
             //assert
             Assert.IsTrue(sql.StartsWith("ALTER TABLE \"dbo\".\"Orders\" DROP COLUMN \"COUNTRY\" CASCADE", StringComparison.OrdinalIgnoreCase), "names should be quoted correctly");
+        }
+
+        [TestMethod]
+        public void TestSqlServerWithIndex()
+        {
+            //arrange
+            var migration = new DdlGeneratorFactory(SqlType.SqlServer).MigrationGenerator();
+
+            var table = MigrationCommon.CreateTestTable("Orders");
+            var orderId = table.DatabaseSchema.AddTable("Customer")
+                .AddColumn<int>("Id").AddPrimaryKey("pk_customer")
+                .AddColumn<int>("OrderId").AddForeignKey("fk_cust_orders", "Orders").AddIndex("idx_orderId");
+
+            //act
+            var sql = migration.DropColumn(orderId.Table, orderId);
+
+            //assert
+
+            Assert.IsTrue(sql.Contains("DROP INDEX [idx_orderId] ON [Customer];"), "should drop index");
+            Assert.IsTrue(sql.Contains("ALTER TABLE [Customer] DROP CONSTRAINT [fk_cust_orders]"), "should drop fk");
+            Assert.IsTrue(sql.Contains("ALTER TABLE [Customer] DROP COLUMN [OrderId];"), "should drop column");
         }
     }
 }
